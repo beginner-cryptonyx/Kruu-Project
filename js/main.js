795e8fd0a3b5a755e6d0c7d135d601acf5e3d1e9
beginner-cryptonyx <mayanksethi242008@gmail.com>
2023-02-04 22:33:58 +0530

    commit #2

--- js/main.js
+++ js/main.js
@@ -1,34 +1,7 @@
 const spans = document.querySelectorAll("span");
-const cards = document.querySelectorAll(".card");
-const booking_indicators = document.querySelectorAll(".booking_indicator");
+// const cards = document.querySelectorAll(".card");
+
 function isMobile() {
-  return window.innerWidth <= 480;
+  return window.innerWidth <= 700;
 }
 
-
-spans.forEach(span => {
-  const text = span.textContent.toLowerCase();
-
-  if (text === "booked") {
-    span.classList.add("booked");
-  } else if (text === "book now") {
-    span.classList.add("BookNow");
-
-  }
-});
-
-
-for (let i = 0; i < cards.length; i++) {
-  cards[i].style.gridColumn = `${(i % 3) + 1}/${(i % 3) + 1}`;
-};
-
-
-
-const elements = document.querySelectorAll(".booked");
-
-
-elements.forEach(element => {
-  if (isMobile()) {
-    span.textContent = "x";
-  }
-});
